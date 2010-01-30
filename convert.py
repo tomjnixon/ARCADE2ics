d7e2f076d7be6f015829915cf2a04b5fd3f852c6
Thomas Nixon <tomjnixon@googlemail.com>
2010-01-30 23:38:03 +0000

    Moved date and time handling to the Event class.

--- convert.py
+++ convert.py
@@ -9,11 +9,6 @@ from compatibility import *
 desc_time_re = re.compile("are at (\S*)")
 desc_room_re = re.compile("in room (\S*)")
 
-date_re = re.compile("(\d+)/(\d+)")
-hour_re = re.compile("[\d:]+[ap]m")
-time_parse_re = re.compile("(\d+):?(\d*)([ap]m)")
-
-
 def get_events(table_str):
     """Split a table up into fields."""
     # Split into lines
@@ -56,46 +51,10 @@ def get_descriptions(descriptions_str):
     return dict((desc["code"], desc) for desc in descriptions)
 
 
-def parse_time(time_str):
-    """Parse a time, returning the minute and the hour.
-    accepts formats: "noon", "XX[ap]m", "XX:XX[ap]m"
-    """
-    if time_str == "noon":
-        return (0, 12)
-
-    match = time_parse_re.match(time_str)
-    hour = int(match.group(1))
-    if match.group(3) == "pm" and hour != 12:
-        hour += 12
-
-    if match.group(2):
-        minute = int(match.group(2))
-    else:
-        minute = 0
-    
-    return (minute, hour)
-    
-
-def get_time(event, descriptions):
-    """Get the time (and date) from an event.
-    If there is no time in the event, try to find one in the descriptions.
-    """
-    day, month = map(int, date_re.search(event["date"]).groups())
-    if "time" in event:
-        minute, hour = parse_time(event["time"])
-    else:
-        description = descriptions.get(event["unit"])
-        if description and "time" in description:
-            minute, hour = parse_time(description["time"])
-        else:
-            minute, hour = None, None
-    return (minute, hour, day, month)
-
-
 def make_event(event, descriptions, time_stamp):
     """Make an iCal event from an ARCADE event."""
     vevent = icalendar.Event()
-    minute, hour, day, month = get_time(event, descriptions)
+    minute, hour, day, month = event.time
     year = time_stamp.year
 
     whole_day = minute is None or hour is None
--- event.py
+++ event.py
@@ -1,6 +1,31 @@
 import os
 from compatibility import *
 
+
+date_re = re.compile("(\d+)/(\d+)")
+hour_re = re.compile("[\d:]+[ap]m")
+time_parse_re = re.compile("(\d+):?(\d*)([ap]m)")
+
+def parse_time(time_str):
+    """Parse a time, returning the minute and the hour.
+    accepts formats: "noon", "XX[ap]m", "XX:XX[ap]m"
+    """
+    if time_str == "noon":
+        return (0, 12)
+
+    match = time_parse_re.match(time_str)
+    hour = int(match.group(1))
+    if match.group(3) == "pm" and hour != 12:
+        hour += 12
+
+    if match.group(2):
+        minute = int(match.group(2))
+    else:
+        minute = 0
+    
+    return (minute, hour)
+
+    
 def get_from_descriptions(name, desc_name, default=None):
     def get(self):
         if hasattr(self, name) and getattr(self, name) is not None:
@@ -10,7 +35,7 @@ def get_from_descriptions(name, desc_name, default=None):
         else:
             return default
     return get
-            
+
 
 class Event(object):
     """A representation of an event from the ARCADE timetable."""
@@ -19,17 +44,22 @@ class Event(object):
     room = None
     descriptions = None
 
+
     def __init__(self, event_str):
         self.event_str = event_str
         self.parse_event(event_str)
+
 
     @classmethod
     def set_descriptions(class, descriptions):
         class.descriptions = descriptions
 
+
     room = property(get_from_descriptions("_room", "room"))
+
         
     time_str = property(get_from_descriprions("_time", "time"))
+
 
     def parse_event(event_str):
         """Parse an event, setting the following 
@@ -37,7 +67,7 @@ class Event(object):
         and possibly "room", and "time"
         """
         tokens = filter(len, event_str.split())
-        self._date = tokens.pop(0)
+        self.date_str = tokens.pop(0)
         self.session = tokens.pop()
         if hour_re.match(tokens[0]):
             self._time = tokens.pop(0)
@@ -45,6 +75,7 @@ class Event(object):
         self.group = tokens.pop(0)
         if tokens:
             self._room = tokens.pop(0)
+
 
     @property
     def summary(self):
@@ -55,3 +86,24 @@ class Event(object):
     @property
     def uid(self):
         return "%s/%s" % (md5(self.event_str), os.getenv("USER"))
+
+
+    @property 
+    def date(self):
+        day, month = map(int, date_re.search(self.date_str).groups())
+        return (day, month)
+
+    
+    @property
+    def time(self):
+        """Get the time (and date) from an event."""
+        day, month = self.date
+
+        if self.time_str:
+            minute, hour = parse_time(self.time_str)
+        else:
+            munite, hour = None, None
+
+        return (minute, hour, day, month)
+
+
